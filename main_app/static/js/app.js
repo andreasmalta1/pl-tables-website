6c58a44ea8110c6a25978900b114aaec7f6049c5
Andreas Calleja <andreascalleja@gmail.com>
2023-06-04 18:03:53 +0200

    Adding dates to POST page

--- main_app/static/js/app.js
+++ main_app/static/js/app.js
@@ -1,3 +1,6 @@
 document.getElementById('start_date').max = max = new Date().toISOString().split("T")[0];
-document.getElementById('end_date').valueAsDate = new Date();
 document.getElementById('end_date').max = max = new Date().toISOString().split("T")[0];
+
+if (!endDate){
+    document.getElementById('end_date').valueAsDate = new Date();
+}
