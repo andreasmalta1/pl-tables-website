5bbdcb53f35fc9cf1c0e3830c5befcfa71361109
Andreas Calleja <andreascalleja@gmail.com>
2023-06-04 21:38:35 +0200

    Add current managers list

--- main_app/routes.py
+++ main_app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, request, flash
+from flask import render_template, request, flash, url_for
 from os import getenv
-from datetime import date
+from datetime import date, datetime
+import requests
 
 from main_app import app
 from main_app.utils import generate_table
@@ -15,7 +16,10 @@ def index():
         start_date = request.form.get("start_date")
         end_date = request.form.get("end_date")
 
-        if end_date < start_date:
+        start_date_check = datetime.strptime(start_date, "%Y-%m-%d")
+        end_date_check = datetime.strptime(end_date, "%Y-%m-%d")
+
+        if end_date_check < start_date_check:
             flash("End date must be after start date", category="error")
             return render_template("index.html")
 
@@ -35,33 +39,39 @@ def index():
 def managers():
     if request.method == "POST":
         manager = request.form.get("manager")
+        manager_start = current_managers[manager]["date_start"]
 
-    for manager in current_managers:
-        date_start = current_managers[manager]["date_start"].split("-")
-        date_today = date.today()
-        d0 = date(int(date_start[2]), int(date_start[1]), int(date_start[0]))
-        delta = date_today - d0
+        data = {"start_date": manager_start, "end_date": "2023-06-04"}
 
-        current_managers[manager]["days_in_charge"] = delta.days
-        current_managers[manager][
-            "manager_url"
-        ] = f"{getenv('API_MANAGER_URL')}{current_managers[manager]['fotmob_id']}.png"
-        current_managers[manager][
-            "nationality_url"
-        ] = f"{getenv('API_CREST_URL')}{NATIONS.get(current_managers[manager]['nationality'])}.png"
-        current_managers[manager][
-            "club_url"
-        ] = f"{getenv('API_CREST_URL')}{TEAMS.get(current_managers[manager]['club'])}.png"
+        response = requests.post(url_for("index", _external=True), data=data)
+        return response.content
 
-    return render_template("managers.html", current_managers=current_managers)
+    if request.method == "GET":
+        for manager in current_managers:
+            date_start = current_managers[manager]["date_start"].split("-")
+            date_today = date.today()
+            d0 = date(int(date_start[0]), int(date_start[1]), int(date_start[2]))
+            delta = date_today - d0
+
+            current_managers[manager]["days_in_charge"] = delta.days
+            current_managers[manager][
+                "manager_url"
+            ] = f"{getenv('API_MANAGER_URL')}{current_managers[manager]['fotmob_id']}.png"
+            current_managers[manager][
+                "nationality_url"
+            ] = f"{getenv('API_CREST_URL')}{NATIONS.get(current_managers[manager]['nationality'])}.png"
+            current_managers[manager][
+                "club_url"
+            ] = f"{getenv('API_CREST_URL')}{TEAMS.get(current_managers[manager]['club'])}.png"
+
+        return render_template("managers.html", current_managers=current_managers)
 
 
 # TODO
-# How many page viewers
-# CSS
-# Add buttons -> since guardiola manager etc, Fergie's time in charge ...
+# Going back multiple seasons is problematic with the API -> Find new API or build own model/API
+# Add Landmark managers buttons (Fergie's time in charge ...)
 # Add tables by season
-# Footer -> Add send email and SupportMe
+# CSS
 # Landing page
-
-# Add managers face
+# Footer -> Add send email and SupportMe
+# How many page viewers
