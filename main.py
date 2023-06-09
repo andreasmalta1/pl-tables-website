23fb3beba0c460c4d0022e85f32d2ee811594666
Andreas Calleja <andreascalleja@gmail.com>
2023-06-09 21:03:25 +0200

    Enabled blueprints

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ from main_app import app
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
+
+# TODO
+# In downloaded images add credits and site url
+# Serialiser (see image)
+
+# Add check to never add already posted matches and never double scrape data (Better to test during the season)
+# Use postgres database instead of sqlite
+# How many page viewers
+# Check about post key viewable in network
+
+# Create readme
+# Document code
+
+# Deploy
