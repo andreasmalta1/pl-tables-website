91d29f0ae404fef9e3814d0be258a22bee814c33
Andreas Calleja <andreascalleja@gmail.com>
2023-06-09 22:14:34 +0200

    Added credits to downloaded images

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
 
 # TODO
-# In downloaded images add credits and site url
-# Serialiser (see image)
-
 # Add check to never add already posted matches and never double scrape data (Better to test during the season)
 # Use postgres database instead of sqlite
 # How many page viewers
