479889cef6eb0c6b3f068eb91a0e4422075f5ba1
Andreas Calleja <andreascalleja@gmail.com>
2023-06-10 16:10:49 +0200

    Updated todo list

--- main.py
+++ main.py
@@ -3,11 +3,7 @@ from main_app import app
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
 
-# TODO
-# https://realpython.com/flask-by-example-part-2-postgres-sqlalchemy-and-alembic/
-# Config file
 # How many page viewers
-# Check about post key viewable in network
 
 # Create readme
 # Document code
