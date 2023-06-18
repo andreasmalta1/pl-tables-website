e6ce54ccf46d50eb98d871f05297de38e7e9a7eb
Andreas Calleja <andreascalleja@gmail.com>
2023-06-18 19:57:52 +0200

    Added favicon

--- main.py
+++ main.py
@@ -2,3 +2,12 @@ from main_app import app
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
+
+
+# Points in other tables not red and bigger
+# Better spacing in seasons
+# BMAC
+# Mobile
+# - Seasons view
+# - Bigger fonts
+# - Bigger tables
