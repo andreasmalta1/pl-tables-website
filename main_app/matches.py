9042428c048a815ce049152c661d0c0c34558135
Andreas Calleja <38852990+andreasmalta1@users.noreply.github.com>
2023-08-08 14:27:43 +0200

    Added functionality to post new matches to db

--- main_app/matches.py
+++ main_app/matches.py
@@ -1,12 +1,9 @@
-from flask import Blueprint, render_template, request, flash, url_for
+from flask import Blueprint, render_template, request, flash,
 from os import getenv
-from datetime import date, datetime
-import requests
 
+from main_app import db
 from main_app.models import Match, CurrentTeams
-from main_app.utils import generate_table, get_teams_info, update_visits
-from main_app.managers import managers_dict
-from main_app.teams import NATIONS
+from main_app.utils import get_teams_info
 
 CREST_URL = getenv("CREST_URL")
 MANAGER_FACE_URL = getenv("MANAGER_FACE_URL")
@@ -20,30 +17,43 @@ matches = Blueprint("matches", __name__)
 
 @matches.route(f"/matches/{POST_KEY}", methods=["GET", "POST"])
 def new_match_results():
-    # Create a view, accessible only using the auth key
-    # Select date, team, team score (home and away) - get season and teams from current teams model
     # Ablilty to post match score or enter a new match
     # First make sure functionality to get post one match is done.
     # Then work to enter multiple matches at once
+    # Check that date exists and team ids is not the same
     # Then CSS
+    # Show all teams when not everyone has played
+
+    current_teams = CurrentTeams.query.all()
 
     if request.method == "GET":
-        current_teams = CurrentTeams.query.all()
         return render_template("post_match_results.html", current_teams=current_teams)
 
-    if request.method == "GET":
-        # Get data from form
-        # Get season as already done
-        # Post to DB
-        current_teams = CurrentTeams.query.all()
+    if request.method == "POST":
+        match_date = request.form["match-day"]
+        home_team_id = request.form["home-team"]
+        home_team_name = (
+            CurrentTeams.query.filter_by(team_id=home_team_id).first().team_name
+        )
+        home_score = request.form["home-team-score"]
+
+        away_team_id = request.form["away-team"]
+        away_team_name = (
+            CurrentTeams.query.filter_by(team_id=away_team_id).first().team_name
+        )
+        away_score = request.form["away-team-score"]
+
+        season = CurrentTeams.query.first().season
+        new_match = Match(
+            season=season,
+            home_team_id=home_team_id,
+            home_team_name=home_team_name,
+            away_team_id=away_team_id,
+            away_team_name=away_team_name,
+            home_score=home_score,
+            away_score=away_score,
+            date=match_date,
+        )
+        db.session.add(new_match)
+        db.session.commit()
         return render_template("post_match_results.html", current_teams=current_teams)
-
-    # # Get all unique seasons present in db
-    # season = CurrentTeams.query.first().season
-
-    # current_table = generate_table(None, None, season)
-    # all_time_table = generate_table(None, None, None)
-
-    # return render_template(
-    #     "index.html", current_table=current_table, all_time_table=all_time_table
-    # )
