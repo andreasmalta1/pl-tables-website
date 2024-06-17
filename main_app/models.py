87a0270336bead56f8b8ea40a170f9abefce3940
Andreas Calleja <38852990+andreasmalta1@users.noreply.github.com>
2024-06-17 17:27:35 +0200

    Updating models

--- main_app/models.py
+++ main_app/models.py
@@ -2,13 +2,19 @@ from main_app import db
 from sqlalchemy.sql import func
 
 
-# Match model
+class Teams(db.Model):
+    name = db.Column(db.String(100), unique=False)
+    crest_url = db.Column(db.String(100), unique=False)
+
+    def __repr__(self):
+        """Return the string representing a match."""
+        return self.team_name
+
+
 class Match(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    home_team_id = db.Column(db.Integer(), unique=False)
-    home_team_name = db.Column(db.String(100), unique=False)
-    away_team_id = db.Column(db.Integer(), unique=False)
-    away_team_name = db.Column(db.String(100), unique=False)
+    # home_team relationship
+    # away_team relationship
     home_score = db.Column(db.Integer(), server_default="0")
     away_score = db.Column(db.Integer(), server_default="0")
     date = db.Column(db.Date(), server_default=func.now())
@@ -20,7 +26,48 @@ class Match(db.Model):
         return f"{self.home_team_name} {self.home_score} - {self.away_score} {self.away_team_name}"
 
 
-# Visit model
+class Nations(db.Model):
+    name = db.Column(db.String(100), unique=False)
+    flag_url = db.Column(db.String(100), unique=False)
+
+    def __repr__(self):
+        """Return the string representing a match."""
+        return self.team_name
+
+
+class Managers(db.Model):
+    name = db.Column(db.String(100), unique=False)
+    face_url = db.Column(db.String(100), unique=False)
+    # nation relationship
+
+    def __repr__(self):
+        """Return the string representing a match."""
+        return self.team_name
+
+
+class ManagerStints(db.Model):
+    # manager relationship
+    # club relationship
+    date_start = db.Column(db.Date(), nullable=False)
+    date_end = db.Column(db.Date(), nullable=True)
+    current = db.Column(db.Boolean, default=False, nullable=False)
+
+    def __repr__(self):
+        """Return the string representing a match."""
+        return self.team_name
+
+
+class PointDeductions(db.Model):
+    # team_relationship
+    points_deducted = db.Column(db.Integer(), unique=False)
+    reason = db.Column(db.Text(), unique=False)
+    season = db.Column(db.String(9), unique=False)
+
+    def __repr__(self):
+        """Return the string representing a match."""
+        return self.team_name
+
+
 class Visit(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_ip = db.Column(db.String(15), unique=False)
@@ -35,13 +82,3 @@ class Visit(db.Model):
         self.user_ip, self.page_name = user_ip, pagename
         db.session.add(self)
         db.session.commit()
-
-
-class CurrentTeams(db.Model):
-    team_name = db.Column(db.String(100), unique=False)
-    team_id = db.Column(db.Integer(), unique=False, primary_key=True)
-    season = db.Column(db.String(9), unique=False)
-
-    def __repr__(self):
-        """Return the string representing a match."""
-        return self.team_name
