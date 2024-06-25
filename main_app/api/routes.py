9519e2a165acf7449afa774775d7ec1bdbf9685d
Andreas Calleja <andreascalleja@gmail.com>
2024-06-25 20:54:58 +0200

    Managerial table skeleton

--- main_app/api/routes.py
+++ main_app/api/routes.py
@@ -1,8 +1,10 @@
 from flask import jsonify
 from sqlalchemy.orm import aliased
+from datetime import date
 
+from app import db
 from api import api_blueprint
-from models import Team, Match, Season
+from models import Team, Match, ManagerStint
 from utils import generate_table
 
 
@@ -31,16 +33,47 @@ def seasons(season):
     return jsonify(standings_dict)
 
 
-@api_blueprint.route("/managers/int<stint_id>", methods=["GET"])
+@api_blueprint.route("/managers/<int:stint_id>", methods=["GET"])
 def managers(stint_id):
-    print(stint_id)
-    return jsonify({"manager_id": stint_id})
-    season = season.replace("-", "/")
+    manager_stint = ManagerStint.query.filter_by(id=stint_id).first()
+    date_start = manager_stint.date_start
+    if manager_stint.current:
+        date_end = date.today()
+    if not manager_stint.current:
+        date_end = manager_stint.date_end
+
+    # date_start = date_start.split("-")
+    # date_end = date_end.split("-")
+
     HomeTeam = aliased(Team, name="home_team")
     AwayTeam = aliased(Team, name="away_team")
 
+    # matches = (
+    #     Match.query.filter(
+    #         db.and_(
+    #             Match.date
+    #             >= date(
+    #                 year=int(date_start[0]),
+    #                 month=int(date_start[1]),
+    #                 day=int(date_start[2]),
+    #             ),
+    #             Match.date
+    #             <= date(
+    #                 year=int(date_end[0]), month=int(date_end[1]), day=int(date_end[2])
+    #             ),
+    #         )
+    #     )
+    #     .join(HomeTeam, HomeTeam.id == Match.home_team_id)
+    #     .join(AwayTeam, AwayTeam.id == Match.away_team_id)
+    #     .add_columns(
+    #         HomeTeam.name.label("home_team_name"),
+    #         Match.home_score,
+    #         AwayTeam.name.label("away_team_name"),
+    #         Match.away_score,
+    #     )
+    #     .all()
     matches = (
-        Match.query.filter_by(season=season)
+        Match.query.filter(db.and_(Match.date >= date_start, Match.date <= date_end))
         .join(HomeTeam, HomeTeam.id == Match.home_team_id)
         .join(AwayTeam, AwayTeam.id == Match.away_team_id)
         .add_columns(
@@ -51,7 +84,8 @@ def managers(stint_id):
         )
         .all()
     )
-    standings = generate_table(matches, season)
+
+    standings = generate_table(matches, None)
     standings_dict = {}
     for team in standings:
         standings_dict[team[0]] = team[1]
--- main_app/static/js/current_managers.js
+++ main_app/static/js/current_managers.js
@@ -1,6 +1,10 @@
+const managersUrl = `${SCRIPT_ROOT}/api/managers`
+
 const managersTable = document.getElementById("currentManagers")
 const managersTableBody = managersTable.getElementsByTagName('tbody')[0];
 const managersRows = Array.from(managersTableBody.getElementsByTagName('tr'));
+const managerBtns = document.querySelectorAll('.down-button');
+const tableDiv = document.getElementById('standings')
 
 function daysElapsedNow(dateString) {
     const inputDate = new Date(dateString);
@@ -26,3 +30,115 @@ managersTableBody.innerHTML = '';
 currentDaysElapsed.forEach(({ row }) => {
     managersTableBody.appendChild(row);
 })
+
+function createTable(data){
+    tableDiv.innerHTML = '';
+    let table = document.createElement('table');
+    table.id = 'pl-table';
+    table.className = 'standing-table';
+
+    let thead = document.createElement('thead');
+    let headerRow = document.createElement('tr');
+
+    let headers = ['#', '', 'Team', 'MP', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'PTS'];
+
+    headers.forEach(headerText => {
+        let th = document.createElement('th');
+        if (headerText === 'Team') {
+            th.className = 'team';
+        }
+        th.textContent = headerText;
+        headerRow.appendChild(th);
+    });
+
+    thead.appendChild(headerRow);
+    table.appendChild(thead);
+
+    // Create the table body
+    let tbody = document.createElement('tbody');
+
+    for (const team in data) {
+        let row = document.createElement('tr');
+        let rkCell = document.createElement('td');
+        rkCell.textContent = data[team]["rk"];
+        row.appendChild(rkCell);
+
+        let logoCell = document.createElement('td');
+        let img = document.createElement('img');
+        img.src = data[team]["url"]
+        img.style.width = '30px'; // Adjust the size as needed
+        logoCell.appendChild(img);
+        row.appendChild(logoCell);
+
+        let teamCell = document.createElement('td');
+        teamCell.textContent = team;
+        row.appendChild(teamCell);
+
+        let stats = ["played", "win", "draw", "loss", "goals_for", "goals_against", "gd", "points"];
+        stats.forEach(stat => {
+            let td = document.createElement('td');
+            td.textContent = data[team][stat];
+            row.appendChild(td);
+        });
+        tbody.appendChild(row);
+    }
+    table.appendChild(tbody);
+    tableDiv.appendChild(table);
+    return table
+}
+
+function sortTable(table){
+    let n = 0
+    let rows, switching, i, x, y, shouldSwitch, dir, switchCount = 0;
+    switching = true;
+    dir = "asc";
+
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+
+            if (dir == "asc") {
+                if (Number(x.innerHTML) > Number(y.innerHTML)) {
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (Number(x.innerHTML) < Number(y.innerHTML)) {
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            switchCount++;
+        } else {
+            if (switchCount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+}
+
+
+managerBtns.forEach(btn => {
+    btn.addEventListener('click', function() {
+        let stintId = btn.name;
+        fetch(`${managersUrl}/${stintId}`)
+        .then(response => response.json())
+        .then(data => {
+            table = createTable(data)
+            sortTable(table)
+        })
+        .catch(error => {
+            console.error('There has been a problem with your fetch operation:', error);
+        });
+    });
+});
