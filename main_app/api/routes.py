dbdfd869552653ff3a08d99103f6fd47a846f5b3
Andreas Calleja <andreascalleja@gmail.com>
2024-06-25 21:00:36 +0200

    Removed commented code

--- main_app/api/routes.py
+++ main_app/api/routes.py
@@ -42,36 +42,9 @@ def managers(stint_id):
     if not manager_stint.current:
         date_end = manager_stint.date_end
 
-    # date_start = date_start.split("-")
-    # date_end = date_end.split("-")
-
     HomeTeam = aliased(Team, name="home_team")
     AwayTeam = aliased(Team, name="away_team")
 
-    # matches = (
-    #     Match.query.filter(
-    #         db.and_(
-    #             Match.date
-    #             >= date(
-    #                 year=int(date_start[0]),
-    #                 month=int(date_start[1]),
-    #                 day=int(date_start[2]),
-    #             ),
-    #             Match.date
-    #             <= date(
-    #                 year=int(date_end[0]), month=int(date_end[1]), day=int(date_end[2])
-    #             ),
-    #         )
-    #     )
-    #     .join(HomeTeam, HomeTeam.id == Match.home_team_id)
-    #     .join(AwayTeam, AwayTeam.id == Match.away_team_id)
-    #     .add_columns(
-    #         HomeTeam.name.label("home_team_name"),
-    #         Match.home_score,
-    #         AwayTeam.name.label("away_team_name"),
-    #         Match.away_score,
-    #     )
-    #     .all()
     matches = (
         Match.query.filter(db.and_(Match.date >= date_start, Match.date <= date_end))
         .join(HomeTeam, HomeTeam.id == Match.home_team_id)
