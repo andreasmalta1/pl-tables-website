92627bff4754ee9001f262d6f8f4eefacfac8acd
Andreas Calleja <andreascalleja@gmail.com>
2024-06-27 17:17:28 +0200

    Some code refactoring

--- main_app/api/routes.py
+++ main_app/api/routes.py
@@ -42,26 +42,7 @@ def stints(stint_id):
     if not manager_stint.current:
         date_end = manager_stint.date_end
 
-    HomeTeam = aliased(Team, name="home_team")
-    AwayTeam = aliased(Team, name="away_team")
-
-    matches = (
-        Match.query.filter(db.and_(Match.date >= date_start, Match.date <= date_end))
-        .join(HomeTeam, HomeTeam.id == Match.home_team_id)
-        .join(AwayTeam, AwayTeam.id == Match.away_team_id)
-        .add_columns(
-            HomeTeam.name.label("home_team_name"),
-            Match.home_score,
-            AwayTeam.name.label("away_team_name"),
-            Match.away_score,
-        )
-        .all()
-    )
-
-    standings = generate_table(matches, None)
-    standings_dict = {}
-    for team in standings:
-        standings_dict[team[0]] = team[1]
+    standings_dict = get_matches_by_day(date_start, date_end)
     return jsonify(standings_dict)
 
 
@@ -109,6 +90,11 @@ def managers(stint_id):
 
 @api_blueprint.route("/dates/<date_start>/<date_end>", methods=["GET"])
 def dates(date_start, date_end):
+    standings_dict = get_matches_by_day(date_start, date_end)
+    return jsonify(standings_dict)
+
+
+def get_matches_by_day(date_start, date_end):
     HomeTeam = aliased(Team, name="home_team")
     AwayTeam = aliased(Team, name="away_team")
 
@@ -129,4 +115,5 @@ def dates(date_start, date_end):
     standings_dict = {}
     for team in standings:
         standings_dict[team[0]] = team[1]
-    return jsonify(standings_dict)
+
+    return standings_dict
