4e6b72bc1439ad669f3628a5364c946aece9e7f2
Andreas Calleja <andreascalleja@gmail.com>
2024-06-27 16:13:52 +0200

    Added custom dates functionality

--- main_app/api/routes.py
+++ main_app/api/routes.py
@@ -89,8 +89,6 @@ def managers(stint_id):
         .first()
     )
 
-    print(type(manager_query.date_start.strftime("%m/%d/%Y")))
-
     manager_info = {
         "name": manager_query.manager_name,
         "face_url": manager_query.manager_face_url,
@@ -107,3 +105,28 @@ def managers(stint_id):
     }
 
     return jsonify(manager_info)
+
+
+@api_blueprint.route("/dates/<date_start>/<date_end>", methods=["GET"])
+def dates(date_start, date_end):
+    HomeTeam = aliased(Team, name="home_team")
+    AwayTeam = aliased(Team, name="away_team")
+
+    matches = (
+        Match.query.filter(db.and_(Match.date >= date_start, Match.date <= date_end))
+        .join(HomeTeam, HomeTeam.id == Match.home_team_id)
+        .join(AwayTeam, AwayTeam.id == Match.away_team_id)
+        .add_columns(
+            HomeTeam.name.label("home_team_name"),
+            Match.home_score,
+            AwayTeam.name.label("away_team_name"),
+            Match.away_score,
+        )
+        .all()
+    )
+
+    standings = generate_table(matches, None)
+    standings_dict = {}
+    for team in standings:
+        standings_dict[team[0]] = team[1]
+    return jsonify(standings_dict)
