c3f50fa5fc03e7362520e8961d653249168a1fe6
Andreas Calleja <38852990+andreasmalta1@users.noreply.github.com>
2024-06-28 13:05:13 +0200

    Updated home page route

--- main_app/home/routes.py
+++ main_app/home/routes.py
@@ -1,32 +1,8 @@
-from flask import render_template, request
-from sqlalchemy.orm import aliased
+from flask import render_template
 
 from home import home_blueprint
-from models import Team, Match, Season
-from utils import generate_table, update_visits
 
 
 @home_blueprint.route("/", methods=["GET"])
 def index():
-    update_visits(request.remote_addr, "home")
-    season = Season.query.first().season
-
-    HomeTeam = aliased(Team, name="home_team")
-    AwayTeam = aliased(Team, name="away_team")
-
-    matches = (
-        Match.query.filter_by(season=season)
-        .join(HomeTeam, HomeTeam.id == Match.home_team_id)
-        .join(AwayTeam, AwayTeam.id == Match.away_team_id)
-        .add_columns(
-            HomeTeam.name.label("home_team_name"),
-            Match.home_score,
-            AwayTeam.name.label("away_team_name"),
-            Match.away_score,
-        )
-        .all()
-    )
-
-    standings = generate_table(matches, season)
-
-    return render_template("main/index.html", standings=standings)
+    return render_template("main/index.html")
