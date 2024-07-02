96655e7d18cfdbabe02d94a0a1e51ddd35951e84
Andreas Calleja <andreascalleja@gmail.com>
2024-07-02 08:26:14 +0200

    New stint admin page

--- main_app/updates/routes.py
+++ main_app/updates/routes.py
@@ -61,7 +61,6 @@ def new_manager():
         return render_template("updates/new_manager.html", nations=nations)
 
     if request.method == "POST":
-        print(request.form.get("nation"))
         manager_name = request.form.get("manager_name")
         face_url = request.form.get("face_url")
         nation_id = request.form.get("nation")
@@ -76,3 +75,38 @@ def new_manager():
         db.session.commit()
 
         return render_template("updates/new_manager.html", manager=new_manager)
+
+
+@updates_blueprint.route("/new-stint", methods=["GET", "POST"])
+@login_required
+def new_stint():
+    if request.method == "GET":
+        managers = Manager.query.all()
+        teams = Team.query.all()
+        return render_template("updates/new_stint.html", managers=managers, teams=teams)
+
+    if request.method == "POST":
+        manager_id = request.form.get("manager")
+        team_id = request.form.get("team")
+        current = request.form.get("current")
+        date_start = request.form.get("start-date")
+        if not current:
+            current = False
+            date_end = request.form.get("end-date")
+
+        if current:
+            current = True
+            date_end = None
+
+        new_stint = ManagerStint(
+            manager_id=manager_id,
+            team_id=team_id,
+            date_start=date_start,
+            date_end=date_end,
+            current=current,
+        )
+
+        db.session.add(new_stint)
+        db.session.commit()
+
+        return render_template("updates/new_stint.html", stint=new_stint)
