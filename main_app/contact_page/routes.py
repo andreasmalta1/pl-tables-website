2319a96bd64c006d297376aafa202857250519bd
Andreas Calleja <38852990+andreasmalta1@users.noreply.github.com>
2024-07-22 17:15:45 +0200

    Fixed contact page route

--- main_app/contact_page/routes.py
+++ main_app/contact_page/routes.py
@@ -1,29 +1,28 @@
 from flask import render_template, request, flash
-from flask_mail import Mail, Message
+from flask_mail import Message
 from os import getenv
 
-import app
 from forms import ContactForm
+from app import mail
 
 from contact_page import contact_page_blueprint
 
 
-@contact_page_blueprint.route("/", methods=["GET"])
+@contact_page_blueprint.route("/", methods=["GET", "POST"])
 def contact_page():
     """
     Generate the contact us page.
     The POST method, sends an email with the form data
     """
     form = ContactForm()
+    if request.method == "GET":
+        return render_template("contact_page/contact_page.html", form=form)
+
     if request.method == "POST":
         # Ensure that form data is valid
         if form.validate() == False:
             flash("Enter a valid email address", category="email_contact")
             return render_template("contact_page/contact_page.html", form=form)
-
-        # Init Mail class
-        mail = Mail()
-        mail.init_app(app)
 
         # Send a message with data collected from form
         msg = Message(
@@ -38,6 +37,3 @@ def contact_page():
         )
         mail.send(msg)
         return render_template("contact_page/contact_page.html")
-
-    if request.method == "GET":
-        return render_template("contact_page/contact_page.html", form=form)
