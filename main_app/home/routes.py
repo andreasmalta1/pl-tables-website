166fa04035e6b4d67da7bdcd8af4e9884beb21f3
Andreas Calleja <38852990+andreasmalta1@users.noreply.github.com>
2024-07-22 10:07:09 +0200

    Updated home page routes with new templates dir

--- main_app/home/routes.py
+++ main_app/home/routes.py
@@ -5,9 +5,9 @@ from home import home_blueprint
 
 @home_blueprint.route("/", methods=["GET"])
 def index():
-    return render_template("main/index.html")
+    return render_template("home/index.html")
 
 
 @home_blueprint.route("/about", methods=["GET"])
 def about():
-    return render_template("main/about.html")
+    return render_template("home/about.html")
