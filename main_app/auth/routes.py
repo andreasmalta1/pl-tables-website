9cac6b6c5586cb548d8981fcb2a1ff8a2a5ce56a
Andreas Calleja <andreascalleja@gmail.com>
2024-07-23 12:25:30 +0200

    Email for each login attempt

--- main_app/auth/routes.py
+++ main_app/auth/routes.py
@@ -1,7 +1,11 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, login_required, logout_user, current_user
+from flask_mail import Message
 from werkzeug.security import check_password_hash
+import os
+import datetime
 
+from app import mail
 from auth import auth_blueprint
 from models import User
 
@@ -18,6 +22,18 @@ def login():
         email = request.form.get("email")
         password = request.form.get("password")
         user = User.query.filter_by(email=email).first()
+
+        msg = Message(
+            "Login Attempt",
+            sender=os.getenv("MAIL_USERNAME"),
+            recipients=[os.getenv("MAIL_USERNAME")],
+        )
+        msg.body = "Login Attempt\nEmail: %s\nTimeStamp: %s\nIP Address: %s" % (
+            email,
+            datetime.datetime.now(),
+            request.remote_addr,
+        )
+        mail.send(msg)
 
         if not user or not check_password_hash(user.password, password):
             return render_template("auth/login.html")
