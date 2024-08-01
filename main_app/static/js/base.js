adbb6f59457cab80fc7bcc7de43b50e47174c166
Andreas Calleja <andreascalleja@gmail.com>
2024-08-02 00:11:50 +0200

    Hiding navbar and footer

--- main_app/static/js/base.js
+++ main_app/static/js/base.js
@@ -1,8 +1,12 @@
 const menuBtn = document.querySelector(".hamburger")
 const mobileMenu = document.querySelector(".mobile-nav")
+const navbarContainer = document.querySelector(".navbar-container")
+const footerContainer = document.querySelector(".footer-container")
 
 
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("is-active")
     mobileMenu.classList.toggle("is-active")
+    navbarContainer.classList.toggle("is-active")
+    footerContainer.classList.toggle("is-active")
 })
