08af6fcd6f96dbdaf0f6f4be04204df5bb78b311
Andreas Calleja <38852990+andreasmalta1@users.noreply.github.com>
2024-08-05 16:45:54 +0200

    Updated base js

--- main_app/static/js/base.js
+++ main_app/static/js/base.js
@@ -1,10 +1,8 @@
 const menuBtn = document.querySelector(".hamburger")
-const mobileMenu = document.querySelector(".mobile-nav")
-const footerContainer = document.querySelector(".footer-container")
+const mobileMenu = document.getElementById("mobileNav")
 
 
 menuBtn.addEventListener("click", () => {
     menuBtn.classList.toggle("is-active")
     mobileMenu.classList.toggle("is-active")
-    footerContainer.classList.toggle("is-active")
 })
