0a52779295f8e649278c36905dc5e0f916f5cf9c
Andreas Calleja <38852990+andreasmalta1@users.noreply.github.com>
2024-08-12 17:20:17 +0200

    Update index.js

--- main_app/static/js/index.js
+++ main_app/static/js/index.js
@@ -1,37 +1,70 @@
-const currentSeasonUrl = `${SCRIPT_ROOT}/api/current-season`
-const deductionsUrl = `${SCRIPT_ROOT}/api/deductions`
-const seasonUrl = `${SCRIPT_ROOT}/api/get-current-season`
+const currentBtn = document.getElementById("down-current")
+const allBtn = document.getElementById("down-all")
+const currentTable = document.getElementById("current-table")
+const allTable = document.getElementById("all-time-table")
 
-const seasonDiv = document.getElementById('standingsTitle')
-const tableDiv = document.getElementById('standings')
-const deductionDiv = document.getElementById('deductions')
+let columnIndex
 
-document.addEventListener("DOMContentLoaded", () => {
-    fetch(`${currentSeasonUrl}`)
-        .then(response => response.json())
-        .then(data => {
-            table = createTable(data)
-            sortTable(table, 0)
-        })
-        .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
-        });
+deletedCells = []
 
-    fetch(`${deductionsUrl}/current`)
-        .then(response => response.json())
-        .then(data => {
-            createPointsDeduction(data)
-        })
-        .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
-        });
+downloadImage = (table) => {
+    let headerRow = table.getElementsByTagName('tr')[0];
+    let headers = headerRow.getElementsByTagName('th');
+
+    for (let i = 0; i < headers.length; i++) {
+        if (headers[i].innerHTML === "") {
+            columnIndex = i;
+            break;
+        }
+    }
     
-    fetch(`${seasonUrl}`)
-        .then(response => response.json())
-        .then(data => {
-            seasonDiv.textContent = `Season ${data.season}`
-        })
-        .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
-        });
-})
+    if (columnIndex !== undefined) {
+        let rowCount = table.rows.length;
+        for (let i = 0; i < rowCount; i++) {
+            deletedCells.push(table.rows[i].cells[columnIndex].innerHTML)
+            table.rows[i].deleteCell(columnIndex);
+        }
+    }
+
+    let row = table.insertRow(-1);
+    let cell = row.insertCell(0);
+
+    cell.innerHTML = "Downloaded from www.pltables.com. Developed by @andreascalleja"
+    cell.colSpan = "10";
+    cell.style.fontSize = "0.75rem"
+
+    html2canvas(table)
+    .then(async function (canvas) {
+        const img = await canvas.toDataURL("image/png")
+        const a = document.createElement('a')
+        a.setAttribute("href", img)
+        a.setAttribute("download", "table.png")
+        a.click()
+        a.remove()
+    })
+
+    rowCount = table.rows.length;
+    table.deleteRow(rowCount - 1);
+
+    if (columnIndex !== undefined) {
+        let rowCount = table.rows.length;
+        for (let i = 0; i < rowCount; i++) {
+            let newCell = table.rows[i].insertCell(columnIndex);
+            if (i == 0){
+                newCell.outerHTML = "<th></th>"
+            } else {
+                newCell.innerHTML = deletedCells[i]
+            }
+        }
+    }
+}
+
+currentBtn.addEventListener("click",
+    function() {
+        downloadImage(currentTable)
+    }) 
+
+allBtn.addEventListener("click",
+    function() {
+        downloadImage(allTable)
+    }) 
