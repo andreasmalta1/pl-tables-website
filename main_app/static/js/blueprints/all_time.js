5a3163c5d4ad79901bec3375b2bbac35948177ca
Andreas Calleja <andreascalleja@gmail.com>
2025-03-08 23:15:51 +0100

    All time table download image

--- main_app/static/js/blueprints/all_time.js
+++ main_app/static/js/blueprints/all_time.js
@@ -1,16 +1,24 @@
 const allTimeTableUrl = `${SCRIPT_ROOT}/api/all-time`
+const downloadTableUrl = `${SCRIPT_ROOT}/api/download-table`
 
-
-const tableDiv = document.getElementById('standings')
+const tableDiv = document.getElementById("standings")
+const downloadBtn = document.getElementById("downBtn")
 
 document.addEventListener("DOMContentLoaded", () => {
-    fetch(`${allTimeTableUrl}`)
-        .then(response => response.json())
-        .then(data => {
-            table = createTable(data)
-            sortTable(table, 0)
-        })
-        .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
-        });
+  fetch(`${allTimeTableUrl}`)
+    .then((response) => response.json())
+    .then((data) => {
+      table = createTable(data)
+      sortTable(table, 0)
+    })
+    .catch((error) => {
+      console.error(
+        "There has been a problem with your fetch operation:",
+        error
+      )
+    })
+})
+
+downloadBtn.addEventListener("click", () => {
+  downloadImage("All Time Premier Legaue Table")
 })
